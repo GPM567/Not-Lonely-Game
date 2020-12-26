4531becfc0c1164add4f6d2b57fe379b36c257c1
GPM567 <hwangminki1010@gmail.com>
2020-12-26 23:35:57 +0900

    Real Real Real Last Commit

--- cogs/help.py
+++ cogs/help.py
@@ -1,4 +1,4 @@
-import discord
+import discord, json
 from discord.ext import commands
 
 
@@ -16,6 +16,15 @@ class Help(commands.Cog):
         embed.add_field(name="!내글목록 (게임명) / !목록 (게임명)", value="내가 작성한 해당 게임의 모집 글 목록을 보거나 글을 삭제합니다.", inline=False)
         embed.add_field(name="!도움", value="이 도움말을 출력합니다.", inline=False)
         await ctx.send(embed=embed)
+
+    @commands.command(name="게임신청", aliases=["게임추가"])
+    async def addGame(self, ctx: commands.Context, *, name: str):
+        with open("../data/addGames.json", 'r') as f:
+            data = json.load(f)
+        if name in data.keys():
+            data[name] += 1
+        else:
+            data[name] = 1
 
 
 def setup(bot: commands.Bot):
