87073b6e8b5bc43ce4847303b2a3566408dec77e
GPM567 <62422715+GPM567@users.noreply.github.com>
2020-12-26 21:46:46 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ for filename in os.listdir("cogs"):
     if filename.endswith(".py"):
         bot.load_extension(f"cogs.{filename[:-3]}")
 
-bot.run("NzkxODk0NjAwNDI3ODk2ODUz.X-VzUQ.iVDIBokYoPAmKtji6FpEQ7OBREU")
+bot.run("token")
