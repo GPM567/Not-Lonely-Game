cc559fa656f45f4326c2264a95610f55e695d4ed
GPM567 <hwangminki1010@gmail.com>
2020-12-26 23:14:05 +0900

    Real Real Last Commit

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from discord.ext import commands
 
 bot = commands.Bot(help_command=None, command_prefix="!")
 
+@bot.event
+async def on_ready():
+    await bot.change_presence(activity=discord.Game(f"!도움 | 같이 플레이(n인큐) 모집 글 정리"))
+
 for filename in os.listdir("cogs"):
     if filename.endswith(".py"):
         bot.load_extension(f"cogs.{filename[:-3]}")
